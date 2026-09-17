a31e9069ede55de72f40fb07c80804600cb54c86
Gaerowes <padhangabiyu@gmail.com>
2026-09-17 13:04:31 +0700

    web

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function ini dijalankan ketika tombol Say Hello ditekan.
+function showMessage() {
+    // alert() menampilkan pesan di browser.
+    alert("Hello! Thank you for visiting my CV.");
+}
